ce3dd80c76584f88e817f829241011028d473465
Doodshoofdaapje <borissanders@hotmail.com>
2023-12-12 23:56:02 +0100

    Working solution for both 1 and 2.

--- Week 3/src/Vertex.java
+++ Week 3/src/Vertex.java
@@ -1,6 +1,29 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class Vertex {
-    private List<Integer> x;
-    private Integer y;
+    public Integer x;
+    public Integer y;
+    public Integer width;
+    public String value;
+
+    public Vertex(Integer x, Integer y, Integer width, String value) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.value = value;
+    }
+
+    public boolean isAdjacentTo(Vertex vertex) {
+        if (isInRange(vertex.x, this.x - 1, this.x + this.width) || isInRange(vertex.x + vertex.width, this.x, this.x + this.width + 1)) {
+            if (isInRange(vertex.y, this.y - 1, this.y + 1)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean isInRange(Integer x, Integer lowerBound, Integer upperBound) {
+        return (x >= lowerBound && x <= upperBound);
+    }
 }
--- Week 3/src/Week3.java
+++ Week 3/src/Week3.java
@@ -3,6 +3,7 @@ import java.io.FileNotFoundException;
 import java.net.URL;
 import java.nio.file.Files;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -12,65 +13,62 @@ import java.util.regex.Pattern;
 
 public class Week3 {
 
-    public static List<String> parseLine(String data) {
-        List<String> row = new ArrayList<>();
-        for (char c : data.toCharArray()) {
-            row.add(".");
-        }
-        Pattern pattern = Pattern.compile("(\\d+|[@#$%^&*=\\+\\/-])");
+    public static List<List<Vertex>> parseLine(String data, Integer rowIndex) {
+        //[@#$%^&*=\\+\\/-])
+        List<Vertex> machineNumbers = parsePattern(data, rowIndex, Pattern.compile("(\\d+)"));
+        List<Vertex> specialCharacters = parsePattern(data, rowIndex, Pattern.compile("([@#$%^&*=\\+\\/-])"));
+
+        return Arrays.asList(machineNumbers, specialCharacters);
+    }
+
+    public static List<Vertex> parsePattern(String data, Integer rowIndex, Pattern pattern) {
+        List<Vertex> vertices = new ArrayList<>();
         Matcher matcher = pattern.matcher(data);
-        Integer index = -1;
+        Integer index = 0;
         while (matcher.find()) {
             String element = matcher.group(1);
-            index = data.indexOf(element, index + 1);
-            for (int j = 0; j < element.length(); j++) {
-                row.set(index + j, element + (element.length() - j -1));
-            }
+            index = data.indexOf(element, index);
+            //System.out.println(String.format("New vertex: x = %d, y = %d, width = %d, val = %s", index, rowIndex, element.length(), element));
+            Vertex vertex = new Vertex(index, rowIndex, element.length(), element);
+            vertices.add(vertex);
+            index += + element.length();
         }
-
-        return row;
+        return vertices;
     }
 
-    public static Integer checkSurroundingCells(List<List<String>> matrix, Integer i, Integer j) {
-        List<String> specialSymbols = new ArrayList<>(List.of("@", "#", "$", "%", "^", "&", "*", "=", "+", "-", "/"));
-        String element = matrix.get(i).get(j);
-        if (element == ".") {
-            return 0;
-        }
-        element = element.substring(0, element.length() - 1);
-        System.out.print("Row " + i + " ");
-        System.out.println(element);
-        if (specialSymbols.contains(element)) {
-            return 0;
-        }
-        for (int k = Math.max(i-1, 0); k <= Math.min(i+1, matrix.size() -1); k++) {
-            for (int l = Math.max(j-1, 0); l <= Math.min(j+1, matrix.get(0).size() -1); l++) {
-                if (k == i && l == j) {
-                    continue;
-                }
-                String cell = matrix.get(k).get(l);
-                cell = cell.substring(0, cell.length() - 1);
-                //System.out.println(String.format("cell %d, %d = %s", k, l, cell));
-                if (specialSymbols.contains(cell)) {
-                    //System.out.println(element);
-                    return Integer.parseInt(element);
-                }
-            }
-        }
-        return 0;
-    }
-
-    public static Integer getMachineNumberSum(List<List<String>> matrix) {
+    public static Integer getMachineNumberSum(List<Vertex> numbers, List<Vertex> symbols) {
         Integer sum = 0;
-        for (int k = 0; k < matrix.size(); k++) {
-            for (int l = 0; l < matrix.get(k).size() -1; l++) {
-                Integer cellValue = checkSurroundingCells(matrix, k, l);
-                if (cellValue > 0) {
-                    String cellAsString = matrix.get(k).get(l);
-                    char lastCharacter = cellAsString.charAt(cellAsString.length() -1);
-                    l += lastCharacter - '0'+1;
+        for (Vertex number : numbers) {
+            for (Vertex symbol : symbols) {
+                if (number.isAdjacentTo(symbol)) {
+                    //System.out.println(String.format("Number %s matched with symbol %s", number.value, symbol.value));
+                    sum += Integer.parseInt(number.value);
+                    break;
                 }
-                sum += cellValue;
+            }
+        }
+
+        return sum;
+    }
+
+    public static Integer getGearRatioSum(List<Vertex> numbers, List<Vertex> symbols) {
+        Integer sum = 0;
+        for (Vertex symbol : symbols) {
+            if (!symbol.value.equals("*")) {
+                continue;
+            }
+            Integer ratio = 1;
+            Integer count = 0;
+            for (Vertex number : numbers) {
+                if (number.isAdjacentTo(symbol)) {
+                    //System.out.println(String.format("Gear is adjacent to %d", Integer.parseInt(number.value)));
+                    ratio *= Integer.parseInt(number.value);
+                    count++;
+                }
+            }
+            if (count > 1) {
+                //System.out.println(String.format("Ratio = %d", ratio));
+                sum += ratio;
             }
         }
         return sum;
@@ -79,17 +77,24 @@ public class Week3 {
     public static void main(String args[])  //static method  
     {  
         try {
-            URL path = Week3.class.getResource("GlassBoxInput.txt");
+            URL path = Week3.class.getResource("Input.txt");
             File inputFile = new File(path.getFile());
             Scanner reader = new Scanner(inputFile);
 
-            List<List<String>> matrix = new ArrayList<>();
+            List<Vertex> machineNumbers = new ArrayList<>();
+            List<Vertex> specialCharacters = new ArrayList<>();
+            Integer index = 0;
             while (reader.hasNextLine()) {
                 String data = reader.nextLine();
-                matrix.add(parseLine(data));
+                List<List<Vertex>> parsedData = parseLine(data, index);
+                machineNumbers.addAll(parsedData.get(0));
+                specialCharacters.addAll(parsedData.get(1));
+                index++;
             }
-            System.out.println(getMachineNumberSum(matrix));
-            //printMatrix(matrix);
+            Integer output = getMachineNumberSum(machineNumbers, specialCharacters);
+            Integer output2 = getGearRatioSum(machineNumbers, specialCharacters);
+            System.out.println(output);
+            System.out.println(output2);
             
             reader.close();
         } catch (FileNotFoundException e) {
@@ -97,13 +102,4 @@ public class Week3 {
             e.printStackTrace();
         }    
     } 
-
-    public static void printMatrix(List<List<String>> matrix) {
-        for (List<String> row : matrix) {
-            for (String elemnt : row) {
-                System.out.print(elemnt);
-            }
-            System.out.println("");
-        }
-    }
 }
